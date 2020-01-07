6b7a6f9181ebc57a729f001df7eac08d038cfe5a
Zak Hyslin <rapscalan@gmail.com>
2020-01-06 17:51:46 -0800

    get all studios test passing

---
+++ __tests__/studios.test.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
+
+describe('Studio routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  let studioArray = [];
+  beforeEach(async() => {
+    studioArray = await Promise.all([
+      Studio.create({ name: 'Miramax', 
+        address: { 
+          city: 'Los Angeles', 
+          state: 'California',
+          country: 'USA'
+        }
+      }),
+      Studio.create({ name: 'Sony',
+        address: {
+          city: 'Hollywood',
+          state: 'California',
+          country: 'USA'
+        }
+      })
+    ]);
+    
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can get all the Studios', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        studioArray = JSON.parse(JSON.stringify(studioArray));
+        studioArray.forEach(studio => {
+          expect(res.body).toContainEqual(studio);
+        });
+      });      
+  });
+});
