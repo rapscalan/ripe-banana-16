f9dd10502b76c6db224f5f6a4cffc50d877fe5f4
Zak Hyslin <rapscalan@gmail.com>
2020-01-08 19:20:10 -0800

    films for a studio passing

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
 
 describe('Studio routes', () => {
   beforeAll(() => {
@@ -15,6 +16,8 @@ describe('Studio routes', () => {
     return mongoose.connection.dropDatabase();
   });
   let studioArray = [];
+  let filmArray;
+
   beforeEach(async() => {
     studioArray = await Promise.all([
       Studio.create({ 
@@ -35,21 +38,24 @@ describe('Studio routes', () => {
       })
     ]);
     
+    filmArray = await Promise.all([
+      Film.create({
+        title: 'The Godfather',
+        studio: studioArray[1]._id,
+        released: 1971,
+        cast: []
+      }),
+      Film.create({
+        title: 'The Godfather II',
+        studio: studioArray[1]._id,
+        released: 1973,
+        cast: []
+      })
+    ]);
   });
 
   afterAll(() => {
     return mongoose.connection.close();
-  });
-
-  it('can get all the Studios', () => {
-    return request(app)
-      .get('/api/v1/studios')
-      .then(res => {
-        studioArray = JSON.parse(JSON.stringify(studioArray));
-        studioArray.forEach(studio => {
-          expect(res.body).toContainEqual(studio);
-        });
-      });      
   });
 
   it('can create a studio', () => {
@@ -72,6 +78,30 @@ describe('Studio routes', () => {
             country: 'USA'
           },
           __v: 0
+        });
+      });
+  });
+
+  it('can get all the Studios', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        studioArray = JSON.parse(JSON.stringify(studioArray));
+        studioArray.forEach(studio => {
+          expect(res.body).toContainEqual(studio);
+        });
+      });      
+  });
+
+  it('can get all the films for a studio', () => {
+    return request(app)
+      .get(`/api/v1/studios/${studioArray[1]._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Sony',
+          address: expect.any(Object),
+          films: expect.any(Array)
         });
       });
   });
